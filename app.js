2ba8cd97d606ddf7e58770af1463571ae1e84da9
fengmk2 <fengmk2@gmail.com>
2012-07-30 01:47:22 +0800

    support max size to 200mb

--- app.js
+++ app.js
@@ -3,41 +3,20 @@ var http = require('http');
 var util = require('util');
 var fs = require('fs');
 var path = require('path');
-var connect = require('connect');
 var nfs = require('./common/nfs');
 var config = require('./config');
 
-var tpl = fs.readFileSync('./index.html');
+var HOME_PAGE = fs.readFileSync('./index.html');
+var MAX_SIZE = 1024 * 1024 * 200; // 200mb
 
-var chars = '0123456789abcdef';
-function mkdirSync(dir) {
-  if (!path.existsSync(dir)) {
-    fs.mkdirSync(dir);
-  }
-}
-
-var store_dir_pre = __dirname + '/files';
-
-mkdirSync(store_dir_pre);
-
-for (var i = 0, l = chars.length; i < l; i++) {
-  var p1 = store_dir_pre + '/' + chars[i];
-  mkdirSync(p1);
-  for (var j = 0; j < l; j++) {
-    var p2 = p1 + '/' + chars[j];
-    mkdirSync(p2);
-  }
-}
-
-var static_handle = connect.static(store_dir_pre, {maxAge: 3600000 * 24 * 365 * 10});
-var max_size = 1024 * 1024 * 20; // 20mb
-
-http.createServer(function(req, res) {
+http.createServer(function handle(req, res) {
   if (req.url === '/store' && req.method.toLowerCase() === 'post') {
-    var content_length = parseInt(req.headers['content-length'], 10);
-    if (isNaN(content_length) || content_length > max_size) {
+    var contentLength = parseInt(req.headers['content-length'], 10);
+    if (isNaN(contentLength) || contentLength > MAX_SIZE) {
       res.writeHead(403, {'content-type': 'text/plain'});
-      return res.end(JSON.stringify({success: false, message: 'max file size is 20mb.'}));
+      res.end(JSON.stringify({success: false, message: 'max file size is 200mb.'}));
+      req.connection.destroy();
+      return;
     }
     // parse a file upload
     var form = new formidable.IncomingForm();
@@ -49,39 +28,25 @@ http.createServer(function(req, res) {
       }
       var name = path.basename(file.path);
       var ext = path.extname(file.name);
-      // var store_path =  '/' + name[0] + '/' + name[1] + '/' + name + '/' + file.name;
       var store_path =  '/' + name[0] + '/' + name[1] + '/' + name + ext;
-      res.writeHead(200, {'content-type': 'text/plain'});
-      res.end(JSON.stringify({success: true, payload: {
-        size: file.size,
-        // url: 'http://' + req.headers.host + '/get' + store_path,
-        url: 'http://' + config.qiniu.domain + store_path,
-        type: file.type
-      }}));
-      nfs.store(store_path, file.path, file.type);
-      // fs.rename(file.path, store_dir_pre + store_path);
+      nfs.store(store_path, file.path, file.type, function () {
+        res.writeHead(200, {'content-type': 'text/plain'});
+        res.end(JSON.stringify({success: true, payload: {
+          size: file.size,
+          // url: 'http://' + req.headers.host + '/get' + store_path,
+          url: 'http://' + config.qiniu.domain + store_path,
+          type: file.type
+        }}));
+        fs.unlink(file.path);
+      });
     });
     return;
-  } else if (req.url === '/') {
-    res.writeHead(200, {'content-type': 'text/html'});
-    res.end(tpl);
-  } else if (req.url.indexOf('/get') === 0) {
-    var key = req.url.substring(req.url.indexOf('/get') + 4);
-    nfs.stat(key, function (err, stat) {
-      if (err) {
-        res.writeHead(500);
-        res.end(err.message);
-        return;
-      }
-      res.writeHead(302, {
-        Location: stat.url
-      });
-      res.end();
-    });
-  } else {
-    static_handle(req, res, function () {
-      res.writeHead(404);
-      res.end('Page not found');
-    });
   }
+
+  if (req.url === '/') {
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.end(HOME_PAGE);
+    return;
+  }
+
 }).listen(config.port);
