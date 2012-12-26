48e6a204249e621c062626cbf0aa5c55f5d148f0
fengmk2 <fengmk2@gmail.com>
2012-12-26 17:48:34 +0800

    add auth test

---
+++ test/auth.test.js
@@ -0,0 +1,15 @@
+/*!
+ * nodebox - test/auth.test.js
+ * Copyright(c) 2012 fengmk2 <fengmk2@gmail.com>
+ * MIT Licensed
+ */
+
+"use strict";
+
+/**
+ * Module dependencies.
+ */
+
+var auth = require('../middleware/auth');
+var should = require('should');
+
